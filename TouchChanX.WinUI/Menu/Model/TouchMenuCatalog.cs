67b1fd4de75d21fd001abae7b5635667595225e0
k1mlka <k1mlka@outlook.com>
2026-07-06 04:09:19 +0800

    微调菜单项和设置项目

--- TouchChanX.WinUI/Menu/Model/TouchMenuCatalog.cs
+++ TouchChanX.WinUI/Menu/Model/TouchMenuCatalog.cs
@@ -44,7 +44,8 @@ internal static class TouchMenuCatalog
         TouchMenuPageId.Function,
         new(1, 2),
         [
-            Toggle("keyboard", "Keyboard", ExtendedSymbol.KeyboardClassic, new(0, 0), isEnabled: false),
+            Toggle("touch-bar", "TouchBar", Symbol.SwitchApps, new(0, 1), isEnabled: false),
+            Toggle("keyboard", "Keyboard", ExtendedSymbol.KeyboardClassic, new(0, 2), isEnabled: false),
             Toggle("touch-to-mouse", "Tap Click", Symbol.TouchPointer, new(1, 0), toolTip: "When you tap, the previous tapped position will be clicked."),
             Navigate("function-back", string.Empty, Symbol.Back, new(1, 1), TouchMenuPageId.Main),
             Toggle("battery", "Battery", ExtendedSymbol.VerticalBattery3, new(1, 2), isEnabled: false),
