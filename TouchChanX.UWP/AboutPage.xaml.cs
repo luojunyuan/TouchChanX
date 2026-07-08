06c8fc9f3eba96fab7c94c863dec8f87a28fafe6
k1mlka <k1mlka@outlook.com>
2026-07-08 15:45:02 +0800

    重构 AboutPage 命令绑定

--- TouchChanX.UWP/AboutPage.xaml.cs
+++ TouchChanX.UWP/AboutPage.xaml.cs
@@ -1,5 +1,4 @@
 using R3;
-using R3.ObservableEvents;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using Windows.ApplicationModel.Core;
@@ -18,8 +17,22 @@ public sealed partial class AboutPage : Page
     public AboutPage()
     {
         InitializeComponent();
-        BindReactiveInteractions();
     }
+
+    public ReactiveCommand RateInStoreCommand => field ??= new ReactiveCommand(async (_, _) =>
+    {
+        var result = await StoreContext.RequestRateAndReviewAppAsync();
+
+        if (result.ExtendedError is not null)
+        {
+            var pfn = Windows.ApplicationModel.Package.Current.Id.FamilyName;
+            var storeUri = new Uri($"ms-windows-store://review/?PFN={pfn}");
+            await Launcher.LaunchUriAsync(storeUri);
+        }
+    });
+
+    public ReactiveCommand OpenSourceCommand => field ??= new ReactiveCommand(
+        async (_, _) => await Launcher.LaunchUriAsync(new Uri(OpenSourceUrl)));
 
     private StoreContext StoreContext => field ??= new Func<StoreContext>(() =>
     {
@@ -31,27 +44,6 @@ public sealed partial class AboutPage : Page
 
         return storeContext;
     })();
-
-    private void BindReactiveInteractions()
-    {
-        RateInStoreCard.Events().Click
-            .SubscribeAwait(async (_, _) => await RateInStoreAsync());
-
-        OpenSourceCard.Events().Click
-            .SubscribeAwait(async (_, _) => await Launcher.LaunchUriAsync(new Uri(OpenSourceUrl)));
-    }
-
-    private async Task RateInStoreAsync()
-    {
-        var result = await StoreContext.RequestRateAndReviewAppAsync();
-
-        if (result.ExtendedError is not null)
-        {
-            var pfn = Windows.ApplicationModel.Package.Current.Id.FamilyName;
-            var storeUri = new Uri($"ms-windows-store://review/?PFN={pfn}");
-            await Launcher.LaunchUriAsync(storeUri);
-        }
-    }
 }
 
 [GeneratedComInterface, Guid("45D64A29-A63E-4CB6-B498-5781D298CB4F")]
