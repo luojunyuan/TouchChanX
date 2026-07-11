3303c7ad3170acb89deeb2a764b635e971ec6c9a
k1mlka <k1mlka@outlook.com>
2026-07-11 12:23:06 +0800

    codebehind 里实现保持 SelectedGame

--- TouchChanX.UWP/HomePage.xaml.cs
+++ TouchChanX.UWP/HomePage.xaml.cs
@@ -4,7 +4,6 @@ using TouchChanX.Persistence;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
 using Windows.Storage.Pickers;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace TouchChanX.UWP;
@@ -91,5 +90,12 @@ public sealed partial class HomePage : Page
             .SelectAwait(async (e, _) => await e.DataView.GetStorageItemsAsync())
             .Select(items => items.OfType<StorageFile>().Select(file => file.Path).ToArray())
             .Subscribe(paths => ViewModel.Dispatch(new GameCommand.AddRange(paths)));
+
+        // 首次 SelectedIndex = 0 必须在 Loaded 之后才生效
+        this.Events().Loaded
+            .AsUnitObservable()
+            .Merge(GameList.Items.Events().VectorChanged.AsUnitObservable())
+            .Where(_ => GameList.Items.Count > 0 && GameList.SelectedIndex == -1)
+            .Subscribe(_ => GameList.SelectedIndex = 0);
     }
 }
